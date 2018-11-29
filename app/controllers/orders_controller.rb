76907538b27c6f61cfff69fe647f34f115d6c52c
Greg <gregory.dopart@gmail.com>
2018-11-29 02:13:04 +0100

    orders controler fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,12 @@ class OrdersController < ApplicationController
 
   def index
     if current_user.admin?
-      @orders_o = Order.where("cast(strftime('%m', created_at) as int) < ?", Time.now.month).order('id DESC')
-      @orders_m = Order.where("cast(strftime('%m', created_at) as int) = ?", Time.now.month).order('id DESC')
+      @orders_o = Order.where('extract(month from created_at) = ?', Time.now.month).order('id DESC')
+      @orders_m = Order.where('extract(month from created_at) = ?', Time.now.month).order('id DESC')
       @month_price = 0 + @orders_m.sum { |order| order.total_price if order.status == 'in progress' || order.status == 'closed' }
     else
-      @orders_o = current_user.orders.where("cast(strftime('%m', created_at) as int) < ?", Time.now.month).order('id DESC')
-      @orders_m = current_user.orders.where("cast(strftime('%m', created_at) as int) = ?", Time.now.month).order('id DESC')
+      @orders_o = current_user.orders.where('extract(month from created_at) = ?', Time.now.month).order('id DESC')
+      @orders_m = current_user.orders.where('extract(month from created_at) = ?', Time.now.month).order('id DESC')
       @month_price = 0 + @orders_m.sum { |order| order.total_price }
     end
   end
